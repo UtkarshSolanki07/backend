26e3cc75ad8bbc621f5829ad2c65439ee7ddf28c
Utkarsh Solanki <solankiut07@gmail.com>
2025-04-19 10:21:25 +0530

    final

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 require ('dotenv').config();
 
+
 const connectDB=async()=>{
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected');
 }
 
